455266d1f998a999c52c67021a894808669b7b51
djacobs7 <daniel@sibblingz.com>
2012-12-19 22:58:27 -0800

    brand new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datatrader::Application.config.secret_token = 'f5bd21564477e3ba2cd66ee5c59b9594d10e06eac3be02f1c158b81382ca47fbe671a83a45b7df0a5dac7487549b7b7b149936e5bdd5d913d6d67b84fa246bb4'
