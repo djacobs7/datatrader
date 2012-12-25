b7051bcc9ea35789dd9e7b250c16b8997eb304fa
pankajkumar229 <pankajkumar229@yahoo.com>
2012-12-24 17:46:21 -0800

    basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Datatrader::Application.routes.draw do
+  devise_for :users
+
   resources :data
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Datatrader::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'data#index'
 
   # See how all your routes lay out with "rake routes"
 
